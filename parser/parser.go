8f9547b8dd10b463cb1ef70317bdb4ab9b974e2e
shengyayun <719048774@qq.com>
2019-09-04 20:22:32 +0800

    typo

--- parser/parser.go
+++ parser/parser.go
@@ -11,7 +11,7 @@ import (
  * 能够解析简单的表达式、变量声明和初始化语句、赋值语句。
  * 它支持的语法规则为：
  *
- * programm -> intDeclare | expressionStatement | assignmentStatement
+ * program -> intDeclare | expressionStatement | assignmentStatement
  * intDeclare -> 'int' Id ( = additive) ';'
  * expressionStatement -> addtive ';'
  * addtive -> multiplicative ( (+ | -) multiplicative)*
